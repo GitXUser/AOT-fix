95efa2c47167a780bd675d6cc37a41b445cae2d8
Acc-ISH <36219454+GitXUser@users.noreply.github.com>
2019-12-19 22:13:34 +0530

    Add files via upload

---
+++ App/projects/shared-module/src/lib/shared-module.module.ts
@@ -0,0 +1,80 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxFlowModule, FlowInjectionToken } from '@flowjs/ngx-flow';
+import Flow from '@flowjs/flow.js';
+import { SharedModuleComponent } from './shared-module.component';
+import { FileUploadComponent} from './components/fileupload/file-upload.component';
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatMenuModule,
+  MatTooltipModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatExpansionModule,
+  MatStepperModule,
+  MatTabsModule,
+  MatTreeModule,
+  MatChipsModule
+} from '@angular/material';
+
+// export { Flow };
+export function MyFlowFactory() {
+  return Flow;
+}
+@NgModule({
+  declarations: [
+    SharedModuleComponent,
+    FileUploadComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    NgxFlowModule,
+    MatToolbarModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatMenuModule,
+  MatTooltipModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatExpansionModule,
+  MatStepperModule,
+  MatTabsModule,
+  MatTreeModule,
+  MatChipsModule
+  ],
+  exports: [SharedModuleComponent, FileUploadComponent],
+  providers: [
+    {
+      provide: FlowInjectionToken,
+      useFactory: MyFlowFactory
+    }
+  ]
+})
+export class SharedModule { }
